2ff451027d38ce7b0ebdcd743ca4743981660444
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-08-24 18:09:26 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "environment" {
   description = "Deployment environment"
   type        = string
-  default     = "dev" 
+  default     = "dev"
 
   validation {
     condition     = contains(["dev", "staging", "prod"], var.environment)
